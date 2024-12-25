d6a1ecc94ef791e443d6d1ded1e0f0fb4c9d1be3
ly <minhy2409@gmail.com>
2024-12-25 16:57:23 +0700

    tro ve update status film

--- src/main/java/com/springboot/CinemaSystem/controller/TicketController.java
+++ src/main/java/com/springboot/CinemaSystem/controller/TicketController.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -40,7 +39,6 @@ public class TicketController {
     private UserDao userDao;
     private FileStorageDao fileStorageDao;
     private RevenueDao revenueDao;
-//    private ExcelExportService excelExportService;
 
     @Autowired
     public TicketController(TicketDao ticketDao, TheaterDao theaterDao, ShowtimeDao showtimeDao, DiscountDao discountDao, UserDao userDao, FileStorageDao fileStorageDao, RevenueDao revenueDao) {
